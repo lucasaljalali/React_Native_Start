dd751d4958352395be2d414c33e10586db85895d
Lucas al Jalali <lucasaljalali@gmail.com>
2022-08-11 11:00:52 +0100

    Tab Navigation

--- app1/App.js
+++ app1/App.js
@@ -11,8 +11,10 @@ import Touchable from './components/Touchable';
 import ModalExample from './components/Modal';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function HomeScreen({navigation}){
   
@@ -85,6 +87,7 @@ function ThirdScreen({route, navigation}){
   )
 };
 
+/*this is the navigation by StackNavigator*/
 export default function App(){
   return(
     <NavigationContainer>
@@ -121,6 +124,42 @@ export default function App(){
   )
 };
 
+/*this is the navigation by Tab Navigator
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Tab.Navigator 
+        initialRouteName='Home Page'
+        screenOptions={{
+          headerStyle: {backgroundColor: '#222',},
+          headerTintColor: '#999',
+          headerTitleStyle: {fontWeight: 'bold',},
+          headerRight: ()=>(
+            <View style={{flexDirection: 'row', width:150, justifyContent:'space-evenly'}}>
+              <Button title="Don't Press" color='#999' onPress={()=>alert('Uhm.. you are curious!')}/>
+            </View>
+          )
+        }}
+      >
+        <Tab.Screen
+          name='Home'
+          component={HomeScreen}
+          options={{title:'Home Screen'}}
+        />
+        <Tab.Screen
+          name='Second'
+          component={SecondScreen}
+          options={{title:'Second Screen'}}
+        />
+        <Tab.Screen
+          name='Third'
+          component={ThirdScreen}
+          options={{title:'Third Screen'}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+};*/
 
 const styles = StyleSheet.create({
   mainContainer: {
