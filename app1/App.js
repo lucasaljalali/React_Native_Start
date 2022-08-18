6009c47e3f4741a11b8b4396c902386b704e0ed4
Lucas al Jalali <lucasaljalali@gmail.com>
2022-08-18 16:14:15 +0100

    Update App.js

--- app1/App.js
+++ app1/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-
+import AsyncStorageCourse from '@react-native-async-storage/async-storage';
 
 export default function App(){
-  
+
+  const[course, setCourse]=useState(null);
+
+  function Store(key, value){
+    AsyncStorageCourse.setItem(key, value);
+  };
+
+  async function Get(key){
+    const value = await AsyncStorageCourse.getItem(key);
+    setCourse(value);
+  };
+
+  Store('01', 'React Native')
+  Store('02', 'JavaScript')
+  Store('03', 'C++')
+  Store('04', 'HTML + CSS')
+
+  Get('04')  
+
   return(
     <View style={styles.mainContainer}>
       <Text>Local Storage - Async-Storage</Text>
+      <Text>Course of {course}</Text>
     </View>
   )
 };
