f211cdd51ea8ef9853d9e19e3e5a6421a215da0f
Lucas al Jalali <lucasaljalali@gmail.com>
2022-08-24 17:23:28 +0100

    Init my own company app

---
+++ EuroboxApp/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { NativeBaseProvider } from "native-base";
+
+import { SignIn } from "./src/screens/SignIn";
+
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+        
+          <SignIn />
+        
+    </NativeBaseProvider>
+  );
+}
