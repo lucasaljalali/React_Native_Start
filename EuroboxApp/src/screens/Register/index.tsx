870e7bed19514b68ff6928d40d952153945c1a92
Lucas al Jalali <lucasaljalali@gmail.com>
2022-08-25 15:25:08 +0100

    finished Register layout

--- EuroboxApp/src/screens/Register/index.tsx
+++ EuroboxApp/src/screens/Register/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState} from 'react';
 import { Keyboard, Platform, TouchableWithoutFeedback } from 'react-native';
 import { Box, Center, Heading, FormControl, VStack, Input, Link, Button, Text, HStack, Image, KeyboardAvoidingView, Icon } from 'native-base';
-import { Ionicons } from '@expo/vector-icons'
+import { Ionicons, MaterialIcons } from '@expo/vector-icons'
 
 export function Register(){
 
@@ -10,24 +10,23 @@ export function Register(){
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const [show, setShow] = useState(false);
+
   return(
     <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} h={{ base: "400px", lg: "auto" }} flex={1}>    
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
         <Center w="100%" flex={1}>
           <Box safeArea p="2" py="8" w="90%" maxW="290">
-          <HStack alignItems={'center'} space={3}>
-              <Image source={require('../../../assets/logo.png')} size={70} alt={'Eurobox logo'}/>
+          <HStack alignItems={'center'} space={2} pl={50} >
+              <Image source={require('../../../assets/logo.png')} size={50} alt={'Eurobox logo'}/>
                 <VStack justifyContent={'center'}>
-                  <Heading size='md' fontWeight="600" color="coolGray.800" _dark={{ color: "warmGray.50" }}>
+                  <Heading size='sm' fontWeight="600" color="coolGray.800" _dark={{ color: "warmGray.50" }}>
                     Sign Up
-                  </Heading>
-                  <Heading mt="1" _dark={{ color: "warmGray.200" }} color="coolGray.600" fontWeight="medium" size="xs">
-                    Let's register you!
                   </Heading>
                 </VStack>  
             </HStack> 
             
-            <VStack space={3} mt="5">
+            <VStack space={2} mt="2">
               <FormControl>
                 <FormControl.Label>First and Last name</FormControl.Label>
                 <Input 
@@ -45,6 +44,7 @@ export function Register(){
                   autoCorrect={false} 
                   textContentType={'telephoneNumber'}   
                   variant="rounded"
+                  placeholder="country code + number"
                   onChangeText={setPhone} 
                 />
                 <FormControl.ErrorMessage>invalid cellphone</FormControl.ErrorMessage>
@@ -65,22 +65,25 @@ export function Register(){
               <FormControl>
                 <FormControl.Label>Password</FormControl.Label>
                 <Input 
-                  type="password" 
+                  type={show ? "text" : "password"}
                   variant="rounded"
+                  InputRightElement={<Icon as={MaterialIcons} name={show ? "visibility" : "visibility-off"} size={5} mr="2" color="muted.400" onPress={() => setShow(!show)}/>}
                   onChangeText={setPassword}
                 />
                 <FormControl.ErrorMessage>invalid password</FormControl.ErrorMessage>
               </FormControl>
               
-              <Button mt="2" colorScheme="warmGray" borderRadius={50} onPress={()=>console.log(email+' '+password)}>
+              <Button mt="4" colorScheme="warmGray" borderRadius={50} onPress={()=>console.log('registered')}>
                 Register
               </Button>
               
-              <HStack mt="6" justifyContent="center" alignItems='center'>
+              <HStack mt="4" justifyContent="center" alignItems='center'>
                 <Text fontSize="sm" color="warmGray" _dark={{ color: "warmGray.200" }}>
                   Register with Google:{" "}
                 </Text>
-                <Icon as={Ionicons} name='logo-google' onPress={()=>console.log('Google')} color="blue.500" size={5} m={1}/>
+                <Button size="sm" py={3} variant="ghost" onPress={()=>console.log('Google')} >
+                  <Icon as={Ionicons} name='logo-google' color="blue.500" size={5}/>
+                </Button>      
               </HStack>
             </VStack>
           </Box>
