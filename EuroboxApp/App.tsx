b4a9ff451892ca5adda55a3a125fd3b899863b40
Lucas al Jalali <lucasaljalali@gmail.com>
2022-08-26 18:34:09 +0100

    Navigation

--- EuroboxApp/App.tsx
+++ EuroboxApp/App.tsx
@@ -4,13 +4,20 @@ import { NativeBaseProvider } from "native-base";
 import { SignIn } from "./src/screens/SignIn";
 import { Register } from "./src/screens/Register";
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
     <NativeBaseProvider>
-        
-          <Register />
-        
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="SignIn" component={SignIn} />
+        </Stack.Navigator>
+      </NavigationContainer>        
     </NativeBaseProvider>
   );
 }
